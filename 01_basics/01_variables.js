d41450b4d6c5361c1819ae18826da90016b87ce9
Teena <t89tina@gmail.com>
2025-01-03 08:25:30 +0000

    completed variables and conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 14456
+let accountEmail = "teena@google.com"
+var accountPassword = "345667"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
